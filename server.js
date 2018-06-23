09296370f94717c45d688ef452b663cf6971667d
huma prathama <huma.elektro@gmail.com>
2018-06-24 03:20:10 +0800

    fix initial to play games

--- server.js
+++ server.js
@@ -1,18 +1,62 @@
 var app = require('express')();
+var fs = require('fs');
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 server.listen(3000);
 
+var cards = [
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+      12,
+      13,
+      14,
+      15,
+      16,
+      17,
+      18,
+      19,
+      20,
+      21,
+      22,
+      23,
+      24,
+      25,
+      26,
+      27,
+      28,
+      29,
+      30
+];
+
+var fullCards = [...cards, ...cards, ...cards, ...cards];
+
 function createRoom() {
-    const id =
-        Math.random()
-            .toString(36)
-            .substring(2, 15) +
-        Math.random()
-            .toString(36)
-            .substring(2, 15);
-    return id;
+      const id =
+            Math.random()
+                  .toString(36)
+                  .substring(2, 15) +
+            Math.random()
+                  .toString(36)
+                  .substring(2, 15);
+      return id;
+}
+
+function shuffle(arr) {
+      for (let i = arr.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+      }
+      return arr;
 }
 
 app.get('/', function(req, res) {
@@ -20,17 +64,85 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', socket => {
-    socket.on('create room', data => {
-        roomID = createRoom();
-        io.emit('create room', {roomID: roomID});
-    });
+      socket.on('create room', data => {
+            roomID = createRoom();
+            socket.join(roomID);
+            io.in(roomID).emit('create room', { roomID: roomID });
+            fs.writeFileSync(
+                  './rooms/' + roomID + '.json',
+                  JSON.stringify({
+                        players: [
+                              {
+                                    name: data.name,
+                                    email: data.email,
+                                    cards: [],
+                                    trash: []
+                              }
+                        ],
+                        dealers: [],
+                        playersNumber: 1
+                  })
+            );
+      });
 
-    socket.on('join room', data => {
-        console.log('join room');
-        io.emit(data.roomID, {type: 'join', data: {name: data.name, email: data.email}});
-    });
+      socket.on('join room', data => {
+            socket.join(data.roomID);
+            let room = fs.readFileSync('./rooms/' + data.roomID + '.json');
+            room = JSON.parse(room);
+            room.players.push({
+                  name: data.name,
+                  email: data.email,
+                  cards: [],
+                  trash: []
+            });
+            room.playersNumber++;
+            fs.writeFileSync(
+                  './rooms/' + data.roomID + '.json',
+                  JSON.stringify(room)
+            );
+            console.log(room.playersNumber);
+            socket.emit('join room', { index: room.playersNumber - 1 });
+            io.in(data.roomID).emit('room', room.players);
+      });
 
-    socket.on('update room', data => {
-        io.emit(data.roomID, { type: 'players', data: data.data });
-    });
+      socket.on('init play', data => {
+            let room = fs.readFileSync('./rooms/' + data.roomID + '.json');
+            room = JSON.parse(room);
+            const shuffleCards = shuffle(fullCards);
+            let player = 0;
+            for (const cards of shuffleCards) {
+                  if (player < room.players.length) {
+                        if (room.players[player].cards.length < 11) {
+                              room.players[player].cards.push(cards);
+                        } else {
+                              player++;
+                              if (room.players.length !== player) {
+                                    room.players[player].cards.push(cards);
+                              } else {
+                                    room.dealers.push(cards);
+                              }
+                        }
+                  } else {
+                        room.dealers.push(cards);
+                  }
+            }
+            fs.writeFileSync(
+                  './rooms/' + data.roomID + '.json',
+                  JSON.stringify(room)
+            );
+            io.in(data.roomID).emit('play', room);
+      });
+
+      socket.on('move', data => {
+            let room = fs.readFileSync('./rooms/' + data.roomID + '.json');
+            io.to(data.roomID).emit('move', data);
+            room = JSON.parse(room);
+            room.players[data.index].cards = data.card;
+            room.players[data.index].trash = data.trash;
+            room.dealers = data.dealers;
+            fs.writeFileSync(
+                  './rooms/' + data.roomID + '.json',
+                  JSON.stringify(room)
+            );
+      });
 });
